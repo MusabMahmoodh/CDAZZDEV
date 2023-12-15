688989a0e46c1a336a81df1bd7fef9dc923c8d43
musabmah <musab.18@cse.mrt.ac.lk>
2023-12-15 09:10:31 +0530

    Refactor code

--- server/src/controllers/BlogPostController.ts
+++ server/src/controllers/BlogPostController.ts
@@ -39,4 +39,31 @@ export const deleteBlogPost = async (req: Request, res: Response) => {
   }
 };
 
+export const updateBlogPost = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { title, content } = req.body;
+
+    const updatedBlogPost = await BlogPostModel.findByIdAndUpdate(id, { title, content }, { new: true });
+
+    res.status(200).json({ message: "Blog post updated successfully", updatedBlogPost });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const getBlogPost = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const blogPost = await BlogPostModel.findById(id).populate("author", "-password");
+
+    res.status(200).json(blogPost);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 export { createBlogPost, getAllBlogPosts };
